3bb733b99c8955ec8f95bf2dbde9fd55e03244cf
Shuang <coding@samwushuang.com>
2025-01-22 11:32:04 -0800

    Went through data types in Rust.(the book)

---
+++ data_types/target/debug/data_types.d
@@ -0,0 +1,1 @@
+/Users/shuang/Documents/GitHub/learnRust/data_types/target/debug/data_types: /Users/shuang/Documents/GitHub/learnRust/data_types/src/main.rs
---
+++ data_types/target/debug/deps/data_types-07ff87a5b829e731.d
@@ -0,0 +1,5 @@
+/Users/shuang/Documents/GitHub/learnRust/data_types/target/debug/deps/data_types-07ff87a5b829e731: src/main.rs
+
+/Users/shuang/Documents/GitHub/learnRust/data_types/target/debug/deps/data_types-07ff87a5b829e731.d: src/main.rs
+
+src/main.rs:
