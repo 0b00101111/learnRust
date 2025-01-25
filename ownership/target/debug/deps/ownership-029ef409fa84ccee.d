daaf422b92e39a3881e13b014714fb0753c80c5c
Sam W <37496494+0b00101111@users.noreply.github.com>
2025-01-24 17:42:29 -0800

    learned ownership

---
+++ ownership/target/debug/deps/ownership-029ef409fa84ccee.d
@@ -0,0 +1,5 @@
+/Users/shuang/Documents/GitHub/learnRust/ownership/target/debug/deps/ownership-029ef409fa84ccee: src/main.rs
+
+/Users/shuang/Documents/GitHub/learnRust/ownership/target/debug/deps/ownership-029ef409fa84ccee.d: src/main.rs
+
+src/main.rs:
---
+++ ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+/Users/shuang/Documents/GitHub/learnRust/ownership/target/debug/ownership: /Users/shuang/Documents/GitHub/learnRust/ownership/src/main.rs
