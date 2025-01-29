63b75b0fd27b56d388e470af697e1387c0510f67
Sam W <37496494+0b00101111@users.noreply.github.com>
2025-01-29 15:23:56 -0800

    learned about slice in Rust

---
+++ slice/target/debug/deps/slice-35b8e26f2d9deefc.d
@@ -0,0 +1,5 @@
+/Users/shuang/Documents/GitHub/learnRust/slice/target/debug/deps/slice-35b8e26f2d9deefc: src/main.rs
+
+/Users/shuang/Documents/GitHub/learnRust/slice/target/debug/deps/slice-35b8e26f2d9deefc.d: src/main.rs
+
+src/main.rs:
---
+++ slice/target/debug/slice.d
@@ -0,0 +1,1 @@
+/Users/shuang/Documents/GitHub/learnRust/slice/target/debug/slice: /Users/shuang/Documents/GitHub/learnRust/slice/src/main.rs
